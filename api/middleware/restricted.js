37f486298c8cb577125c2d6fcaac5560032471f1
Colten Empey <colten@empey.me>
2022-05-30 12:23:22 -0600

    finished

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,22 @@
+const jwt = require("jsonwebtoken")
+const {jwtSecret} = require('../auth/secrets')
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+  if(!token){
+    next({status: 401, message: "token required"});
+  }
+  else{
+    jwt.verify(token, jwtSecret, (err, decodedToken)=>{
+      if(err){
+        next({status: 401, message: "token invalid"})
+      }
+      else{
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
+
+  }
   /*
     IMPLEMENT
 
