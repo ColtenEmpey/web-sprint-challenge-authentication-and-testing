7a72f2738e7e7c3389b0c31a477b641e19e22ead
Colten Empey <colten@empey.me>
2022-05-30 11:10:57 -0600

    Register is working

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,13 +1,25 @@
 const router = require('express').Router();
+const bycript = require("bcryptjs")
+const {checkUsernameFree, checkUsernameExists, checkPasswordLength} = require("../middleware/authMiddleware")
+const User = require("../users/users-model.js")
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
+router.post('/register', checkUsernameFree, (req, res, next) => {
+  const {username, password} = req.body
+  const hash = bycript.hashSync(password, 8)
+  User.add({username, password: hash})
+      .then(saved =>{
+        console.log(saved)
+        res.status(200).json(saved)
+      })
+      .catch(err =>{
+        next(err)
+      })
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
     DO NOT EXCEED 2^8 ROUNDS OF HASHING!
 
-    1- In order to register a new account the client must provide `username` and `password`:
+    DONE 1- In order to register a new account the client must provide `username` and `password`:
       {
         "username": "Captain Marvel", // must not exist already in the `users` table
         "password": "foobar"          // needs to be hashed before it's saved
@@ -21,16 +33,26 @@ router.post('/register', (req, res) => {
         "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
       }
 
-    3- On FAILED registration due to `username` or `password` missing from the request body,
+     3- On FAILED registration due to `username` or `password` missing from the request body,
       the response body should include a string exactly as follows: "username and password required".
 
-    4- On FAILED registration due to the `username` being taken,
+     4- On FAILED registration due to the `username` being taken,
       the response body should include a string exactly as follows: "username taken".
   */
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', checkUsernameExists, (req, res, next) => {
   res.end('implement login, please!');
+  const {password} = req.body
+    if(bycript.compareSync(password, req.user.password)){
+
+
+      req.session.user = req.user
+      res.json({ message: `Welcome ${req.user.username}`})
+    }
+    else{
+      next({status: 401, message: 'Invalid credentials'})
+    }
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
---
+++ api/middleware/authMiddleware.js
@@ -0,0 +1,67 @@
+const User = require("../users/users-model")
+
+function restricted(req, res, next) {
+  if(req.session.user){
+    next()
+  }
+  else{
+    res.status(401).json({message: "You shall not pass!"})
+  }
+}
+
+async function checkUsernameFree(req, res, next) {
+  const {username, password} = req.body
+  if(username && password){
+    try {
+        const users = await User.findBy({username: req.body.username })
+        if(!users.length) next()
+        else{
+          next({message : "username taken", status : 422})
+        }
+      
+    }
+    catch(err){
+      next(err)
+    }
+  }
+  else {next({message: "username and password required", status: 422})}
+}
+
+
+
+async function checkUsernameExists(req, res, next) {
+  try {
+    const user = await User.findBy({username: req.body.username})
+    if(user.length){ 
+      req.user = user[0]
+      next()}
+    else{
+      next({message : "Invalid credentials", status : 401})
+    }
+  }
+  catch(err){
+    next(err)
+  }
+}
+
+/*
+  If password is missing from req.body, or if it's 3 chars or shorter
+
+  status 422
+  {
+    "message": "Password must be longer than 3 chars"
+  }
+*/
+async function checkPasswordLength(req, res, next) {
+  if(!req.body.password || req.body.password.length < 3){
+    next({message: "Password must be longer than 3 chars", status: 422})
+  }
+  else{
+    next()
+  }
+}
+
+module.exports = {restricted,
+     checkUsernameFree,
+     checkUsernameExists,
+     checkPasswordLength}
